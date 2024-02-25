2a0dffab0e3954d0cfd088a5518674694b0452aa
Leonardo Albano <leonardoalbanom@yahoo.com.br>
2024-02-25 10:47:33 -0300

    descrições para melhor entendimento

--- src/database/mysqlConnection.ts
+++ src/database/mysqlConnection.ts
@@ -1,14 +1,18 @@
+// Importa as bibliotecas mysql e Pool do módulo 'mysql2/promise'
 import mysql, { Pool } from 'mysql2/promise';
 
+// Cria uma instância do Pool de conexões do MySQL
 const mysqlConnection: Pool = mysql.createPool({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Asdf1346!",
-    database: "fornecedor",
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+    // Configurações da conexão com o banco de dados
+    host: "localhost",          // Endereço do banco de dados
+    port: 3306,                 // Porta de conexão
+    user: "root",               // Nome de usuário
+    password: "Asdf1346!",      // Senha do usuário
+    database: "fornecedor",     // Nome do banco de dados
+    waitForConnections: true,   // Espera por conexões disponíveis quando a conexão atingir o limite máximo
+    connectionLimit: 10,        // Número máximo de conexões permitidas
+    queueLimit: 0               // Número máximo de conexões em fila (0 indica ilimitado)
 });
 
+// Exporta a instância do Pool de conexões para que possa ser utilizada em outros arquivos
 export default mysqlConnection;
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,26 @@
+// Importa o framework Express
 import express from "express";
+
+// Importa o módulo CORS para permitir requisições de origens diferentes
 import cors from "cors";
+
+// Importa as rotas relacionadas aos usuários do arquivo "./routes/user.routes"
 import userRoutes from "./routes/user.routes";
 
+// Cria uma instância do aplicativo Express
 const app = express();
+
+// Define a porta em que o servidor irá escutar as requisições
 const PORT = 3000;
 
+// Middleware para permitir o uso de JSON nas requisições
 app.use(express.json());
+
+// Middleware para habilitar o CORS (Cross-Origin Resource Sharing)
 app.use(cors());
 
+// Define que as requisições para a raiz ("/") serão tratadas pelas rotas relacionadas aos usuários
 app.use("/", userRoutes);
+
+// Inicia o servidor Express para escutar as requisições na porta especificada
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
